971b6e94f1cd13f6c77a60ee5da6d4c0adaa5ea6
arissugiyono <arissugiyonotkj3@gmail.com>
2026-02-09 20:50:34 +0700

    add : Swipe to delete Expense

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -6,7 +6,9 @@ import 'package:expense_tracker/models/expense.dart';
 final formatter = DateFormat.yMd();
 
 class NewExpense extends StatefulWidget {
-  const NewExpense({super.key});
+  const NewExpense({super.key, required this.onAddExpense});
+
+  final Function(Expense expense) onAddExpense;
 
   @override
   State<NewExpense> createState() => _NewExpenseState();
@@ -61,8 +63,17 @@ class _NewExpenseState extends State<NewExpense> {
           );
         },
       );
+      return;
     }
-    return;
+    widget.onAddExpense(
+      Expense(
+        title: _titleController.text,
+        amount: enteredAmount,
+        date: _selectedDate!,
+        category: _selectedCategory,
+      ),
+    );
+    Navigator.pop(context);
   }
 
   @override
@@ -75,7 +86,7 @@ class _NewExpenseState extends State<NewExpense> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.all(16),
+      padding: EdgeInsets.fromLTRB(16, 70, 16, 16),
       child: Column(
         children: [
           TextField(
